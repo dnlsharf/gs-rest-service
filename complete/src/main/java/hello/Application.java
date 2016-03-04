13829655a4b5db7bf427fab81e50bc6c084adbc7
Danil Sharafutdinov <dnlsharf@gmail.com>
2016-03-04 13:05:19 -0800

    Added a comment how to start the app.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,6 +3,9 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// To run the app: mvn spring-boot:run
+// OR java -jar target/gs-rest-service-0.1.0.jar
+// To access your app in a browser: http://localhost:8080/greeting?name=User
 @SpringBootApplication
 public class Application {
 
